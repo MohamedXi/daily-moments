525dfd84780177fe3e433ef16ed089382a8cd32a
Ismael Mohamed <mohamed-ismael@outlook.fr>
2022-08-01 10:52:11 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  setupIonicReact
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { homeOutline, settingsOutline } from 'ionicons/icons';
+import { Redirect, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Settings from './pages/Settings';
+
+setupIonicReact();
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/settings" component={Settings} />
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+        </IonRouterOutlet>
+        <IonTabBar slot='bottom'>
+          <IonTabButton tab="home" href="/home">
+            <IonIcon icon={homeOutline} />
+            <IonLabel>Home</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="settings" href="/settings">
+            <IonIcon icon={settingsOutline} />
+            <IonLabel>Settings</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,31 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage, IonTitle,
+  IonToolbar
+} from '@ionic/react';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Settings</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Settings</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className='ion-padding'>
+          This is the settings page.
+        </IonContent>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
